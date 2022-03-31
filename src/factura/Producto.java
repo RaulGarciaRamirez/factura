7743ee268925133dcdc8d689ca1a486cddac84bb
Raúl <Ral@RalPC>
2022-03-31 19:06:23 +0200

    Clase Producto creada

--- src/factura/Producto.java
+++ src/factura/Producto.java
@@ -1,5 +1,34 @@
 package factura;
 
 public class Producto {
+	private String nombre;
+	private float precio;
+	private int cantidad;
 
+	public Producto(String nombre, float precio, int cantidad) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.cantidad = cantidad;
+	}
+
+	
+	public float precioTotal() {
+		return precio * cantidad;
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Producto [nombre=");
+		builder.append(nombre);
+		builder.append(", precio=");
+		builder.append(precio);
+		builder.append(", cantidad=");
+		builder.append(cantidad);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 }

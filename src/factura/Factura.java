23a2e1027ba61008c9ea9c192b7eaf9ad0531f92
Raúl <Ral@RalPC>
2022-03-31 19:11:18 +0200

    Clase Factura creada

---
+++ src/factura/Factura.java
@@ -0,0 +1,32 @@
+package factura;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factura {
+	private List<Producto> productos;
+	
+	public Factura() {
+		productos = new ArrayList<>();
+	}
+	
+	public void meterProducto(Producto p) {
+		productos.add(p);
+	}
+	
+	public float totalFactura() {
+		float total = 0;
+		
+		for (Producto producto : productos) {
+			total += producto.precioTotal();
+		}
+		
+		return total;
+	}
+	
+	public float aplicarIva(float iva) {
+		float total = totalFactura();
+		total += total * iva;
+		return total;
+	}
+}
